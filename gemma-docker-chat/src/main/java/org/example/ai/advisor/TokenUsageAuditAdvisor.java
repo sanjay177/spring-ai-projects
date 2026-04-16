8bdba2ff34f29bdf760ae02166e390187f69bcbc
Sanjay Rawat <sanjayrawat177@yahoo.com>
2026-04-15 18:52:39 -0700

    Add Advisor

---
+++ gemma-docker-chat/src/main/java/org/example/ai/advisor/TokenUsageAuditAdvisor.java
@@ -0,0 +1,34 @@
+package org.example.ai.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClientRequest;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisor;
+import org.springframework.ai.chat.client.advisor.api.CallAdvisorChain;
+import org.springframework.ai.chat.metadata.Usage;
+import org.springframework.ai.chat.model.ChatResponse;
+
+@Slf4j
+public class TokenUsageAuditAdvisor implements CallAdvisor {
+    @Override
+    public ChatClientResponse adviseCall(ChatClientRequest chatClientRequest, CallAdvisorChain callAdvisorChain) {
+        ChatClientResponse chatClientResponse = callAdvisorChain.nextCall(chatClientRequest);
+        ChatResponse chatResponse = chatClientResponse.chatResponse();
+        assert chatResponse != null;
+        Usage usage = chatResponse.getMetadata().getUsage();
+        log.info("Prompt tokens used: " + usage.getPromptTokens());
+        log.info("Completion tokens used: " + usage.getCompletionTokens());
+        log.info("Total tokens used: " + usage.getTotalTokens());
+        return chatClientResponse;
+    }
+
+    @Override
+    public String getName() {
+        return "TokenUsageAuditAdvisor";
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
--- gemma-docker-chat/src/main/java/org/example/ai/config/ChatClientConfig.java
+++ gemma-docker-chat/src/main/java/org/example/ai/config/ChatClientConfig.java
@@ -1,6 +1,8 @@
 package org.example.ai.config;
 
+import org.example.ai.advisor.TokenUsageAuditAdvisor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +15,8 @@ public class ChatClientConfig {
                 You are an AI assistant who can help with sports related questions and provide information about sports events, teams, players, and statistics.
                 If a user asks for help with anything outside these topics kindly respond with "Sorry, I can only assist with sports related questions."
                 """)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(new TokenUsageAuditAdvisor())
                 .build();
     }
 }
